209587d530edf7e75e18fa9a3466b8232a3af9e7
Stanley <steven.bradn07@gmail.com>
2022-04-09 19:34:56 +0200

    Ajouts de murs exterieurs

--- src/Jeu.java
+++ src/Jeu.java
@@ -13,8 +13,10 @@ public class Jeu {
     public void genererPlateau(){
         plateau = new Cellule[taillePlateau][taillePlateau];
 
+//      MURS CONTOUR
         for (int i=0;i<taillePlateau;i++){
             int dernierePosition = taillePlateau-1;
+
 
 
             plateau[0][i].setMurHaut(true);
@@ -24,8 +26,26 @@ public class Jeu {
             plateau[i][dernierePosition].setMurGauche(true);
         }
 
-        plateau[]
+//      MURS PRES DU CONTOUR
+
+        //        Murs du haut
+        plateau[0][2].setMurDroit(true);
+        plateau[0][11].setMurDroit(true);
+
+        //        Murs à droite
+        plateau[3][taillePlateau].setMurBas(true);
+        plateau[8][taillePlateau].setMurBas(true);
+
+        //        Murs en bas
+        plateau[taillePlateau][4].setMurDroit(true);
+        plateau[taillePlateau][13].setMurDroit(true);
+
+        //        Murs à gauche
+        plateau[3][0].setMurBas(true);
+        plateau[11][0].setMurBas(true);
     }
+
+//      TODO: MURS INTERNES (AD et Richard)
 
 
 }
